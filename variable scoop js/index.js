4c416cf432ede4e3c6080c4486fcb01dac07c6ea
abdifatah286 <uzairhussein0@gmail.com>
2025-07-14 17:30:13 +0300

    first commit

---
+++ variable scoop js/index.js
@@ -0,0 +1,73 @@
+// variable scoop= is where is recognised and accessible (local vs global);
+//let x=1;
+//let x =2;
+
+// local
+
+ /*function2()
+
+
+function function1(){
+
+let x=1;
+
+console.log(x);
+}
+function function2(){
+
+let x=2;
+
+console.log(x);
+}*/
+
+
+ /*function1()
+
+
+function function1(){
+
+let x=1;
+
+console.log(y);
+}
+function function2(){
+
+let x=2;
+
+console.log(x);
+}*/
+// global scope
+
+/*let x=3;
+ function1();
+
+function function1(){
+
+console.log(x);
+}
+
+
+function function2(){
+
+
+
+console.log(x);
+}*/
+
+// if you use both
+let x =4;
+ function2()
+
+
+function function1(){
+
+let x=1;
+
+console.log(x);
+}
+function function2(){
+
+
+
+console.log(x);
+}
